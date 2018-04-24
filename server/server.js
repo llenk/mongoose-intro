85d941165afc369e94d134d2a1ff4606f6f02174
llenk <ellenkeal@gmail.com>
2018-04-24 16:16:37 -0500

    finish lecture

--- server/server.js
+++ server/server.js
@@ -1,43 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-let books = [{
-    title: "A Tale of Two Cities",
-    author: "Charles Dickens",
-    edition: 2,
-    published: '1859-05-20'
-  },
-  {
-    title: "Murder on the Orient Express",
-    author: "Agatha Christie",
-    edition: 2
-  },
-  {
-    title: "Snow Crash",
-    author: "Neal Steve",
-    edition: 1.2,
-    ratings: [
-      {score: 5},
-      {score: 1}
-    ]
-  },
-  {
-    title: "Catcher in the Rye",
-    author: "Someone",
-    edition: 1,
-    ratings: [
-      {score: 2},
-      {score: 5},
-      {score: 3.5},
-      {score: 4}
-    ]}
-];
+const Book = require('./models/book.schema');
+
+//CONNECTING TO MONGO START
+const databaseUrl = 'mongodb://localhost:27017/library';
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', () => {
+    console.log(`mongoose connected to ${databaseUrl}`);
+});
+mongoose.connection.on('error', (error) => {
+    console.log('mongoose connection error', error);
+});
+//CONNECTING TO MONGO END
 
 app.get('/book', (req, res) => {
-    res.send(books);
+    Book.find({})
+        .then((dataFromDatabase) => {
+            console.log('data from database', dataFromDatabase);
+            res.send(dataFromDatabase);
+        })
+        .catch((error) => {
+            console.log('error with Book.find', error);
+            res.send(500);
+        });
 });
 
 app.listen(PORT, () => {
